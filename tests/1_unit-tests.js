e312c124565d7c9c3145314a0c916ced1b66ebd4
nicolas-carrasco53 <hijomili9@gmail.com>
2025-08-15 23:19:16 -0300

    soy puto

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -3,7 +3,7 @@ const assert = chai.assert;
 
 suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
-    // #1 - TEST CORREGIDO (SOLO ESTE BLOQUE MODIFICADO)
+    // #1 - Use assert.isNull() or assert.isNotNull() to make the tests pass
     test('#isNull, #isNotNull', function () {
       assert.isNull(null, 'This is an optional error description - e.g. null is null');
       assert.isNotNull(1, '1 is not null');
