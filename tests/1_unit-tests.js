5d60d2162850293e1229a961a5f767f09c9df441
nicolas-carrasco53 <hijomili9@gmail.com>
2025-08-16 14:48:47 -0300

    otraves

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -3,15 +3,23 @@ const assert = chai.assert;
 
 suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
-    /** 1 - Use assert.isNull() or assert.isNotNull() to make the tests pass. **/
+
+
+// #1 - Prueba corregida
+test('#isNull, #isNotNull', function () {
+  assert.isNull(null, 'This is an optional error description - e.g. null is null');
+  assert.isNotNull(1, '1 is not null');
+});
+
+ /** 1 - Use assert.isNull() or assert.isNotNull() to make the tests pass. 
 test('#isNull, #isNotNull', function() {
   assert.isNull(
     null,
     'this is an optional error description - e.g. null is null'
   );
   assert.isNotNull(1, '1 is not null');
-});
-    // #2
+});**/
+	  // #2
     test('#isDefined, #isUndefined', function () {
       assert.fail(null, 'null is not undefined');
       assert.fail(undefined, 'undefined IS undefined');
