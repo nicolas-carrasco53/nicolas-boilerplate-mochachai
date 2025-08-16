c15d29e5d24502ef1ab86c9e84a73414298eb78d
nicolas-carrasco53 <hijomili9@gmail.com>
2025-08-16 17:34:05 -0300

    primer comentario

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -10,16 +10,7 @@ test('#isNull, #isNotNull', function () {
   assert.isNull(null, 'This is an optional error description - e.g. null is null');
   assert.isNotNull(1, '1 is not null');
 });
-
- /** 1 - Use assert.isNull() or assert.isNotNull() to make the tests pass. 
-test('#isNull, #isNotNull', function() {
-  assert.isNull(
-    null,
-    'this is an optional error description - e.g. null is null'
-  );
-  assert.isNotNull(1, '1 is not null');
-});**/
-	  // #2
+// #2
     test('#isDefined, #isUndefined', function () {
       assert.fail(null, 'null is not undefined');
       assert.fail(undefined, 'undefined IS undefined');
