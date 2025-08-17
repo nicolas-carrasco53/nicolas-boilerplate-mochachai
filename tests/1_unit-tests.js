3bda1154f2bacca038574439983cabfc12ec59ad
nicolas-carrasco53 <hijomili9@gmail.com>
2025-08-16 21:22:02 -0300

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -26,12 +26,12 @@ test('#isOk, #isNotOk', function() {
 }); 
 // #4
     test('#isTrue, #isNotTrue', function () {
-      assert.fail(true, 'true is true');
-      assert.fail(!!'double negation', 'Double negation of a truthy value is true');
-      assert.fail({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
+      assert.istrue(true, 'true is true');
+      assert.istrue(!!'double negation', 'Double negation of a truthy value is true');
+      assert.istrue({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
     });
   });
- 
+ });
   // -----------------------------------------------------------------------------
 
   suite('Equality', function () {
