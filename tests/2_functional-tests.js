e77437ec13bdf6c3229937124746b39a262f4341
nicolas-carrasco53 <hijomili9@gmail.com>
2025-08-19 16:08:55 -0300

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -90,7 +90,15 @@ suite('Functional Tests', function () {
 
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
+      chai
+        .request(server)
+        .put('/travellers') 
+        .send({ surname: 'da Verrazzano' })
+        .end(function(err, res) {
+         assert.equal(res.status, 200, 'response status should be 200');
+         assert.equal(res.type, 'application/json', 'Response should be json');
+         assert.equal(res.body.name, 'Giovanni');
+         assert.equal(res.body.surname, 'da Verrazzano');
       done();
     });
   });
