f7e362c6dbf279416e579cd60541fea8b082b5a8
nicolas-carrasco53 <hijomili9@gmail.com>
2025-08-19 11:56:36 -0300

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -23,12 +23,12 @@ suite('Unit Tests', function () {
     assert.isOk(true, 'true is truthy');
   });
 
- /** // #4 - ÚNICA VERSIÓN (elimina cualquier duplicado)
+ // #4 - ÚNICA VERSIÓN (elimina cualquier duplicado)
   test('#isTrue, #isNotTrue', function () {
     assert.isTrue(true, 'true is true');
     assert.isTrue(!!'double negation', 'Double negation of a truthy value is true');
     assert.isNotTrue({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
-  });**/
+  });
  
 suite('Equality', function () {
     // #5
