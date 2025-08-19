7af7ae581ee8a4015a9036555a0c0ae2838811db
nicolas-carrasco53 <hijomili9@gmail.com>
2025-08-19 18:16:21 -0300

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -92,7 +92,7 @@ suite('Functional Tests', function () {
         .send({ surname: 'da Verrazzano' })
         .end(function (err, res) {
           assert.equal(res.status, 200, 'response status should be 200');
-          assert.equal(res.type, 'application/json', 'Response should be Giovanni');
+          assert.equal(res.type, 'application/json', 'Response should be json');
           assert.equal(res.body.name, 'Giovanni');
           assert.equal(res.body.surname, 'da Verrazzano');
           done();
