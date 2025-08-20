db0054bcbc88a524502dd8f2f2b0b747f1ea49a6
nicolas-carrasco53 <hijomili9@gmail.com>
2025-08-20 14:27:53 -0300

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -106,7 +106,7 @@ suite('Functional Tests', function () {
               browser.assert.success();
               browser.assert.text('span#name', 'Cristoforo');
               browser.assert.text('span#surname', 'Colombo');
-              browser.assert.elements('span#dates', 1);
+              browser.assert.elements('span#dates',);
               done();
             });
           });
