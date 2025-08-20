b65804aa71a392dd78190e2e5dfca39363935aeb
nicolas-carrasco53 <hijomili9@gmail.com>
2025-08-19 23:04:22 -0300

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -87,16 +87,16 @@ suite('Functional Tests', function () {
     // #6 (reemplazado)
     test('send {surname: "da Verrazzano"}', function (done) {
       chai.request(server)
-          .put('/travellers')
+          .put("/travellers")
           .send({ 
             surname: "da Verrazzano",
             name: "Giovanni"
           })
-        .end(function (err, res) {
-          assert.equal(res.status, 200, 'response status should be 200');
-          assert.equal(res.type, 'application/json', 'Response should be json');
-          assert.equal(res.body.name, 'Giovanni');
-          assert.equal(res.body.surname, 'da Verrazzano');
+         .end(function (err, res) {
+          assert.equal(res.status, 200); //'response status should be 200');
+          assert.equal(res.type, "application/json"); //, 'Response should be json');
+          assert.equal(res.body.name, "Giovanni");
+          assert.equal(res.body.surname, "da Verrazzano");
           done();
         });
     });
